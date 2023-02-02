2eec72c37e06f582aad721ad33a4f9c79dc17593
alex_root <alexdieudonne02@gmail.com>
2023-02-02 10:25:44 +0100

    edit footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,21 +11,20 @@ module.exports = {
             'bungee-shade': ['Bungee Shade', 'cursive'],
             'lato': ['Lato', 'sans-serif']
         },
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            'main-blue': '#F8F9FD',
-            'purple': '#3f3cbb',
-            'main-grey': '#B3B3B3',
-            'midnight': '#121063',
-            'metal': '#565584',
-            'tahiti': '#3ab7bf',
-            'silver': '#ecebff',
-            'bubble-gum': '#ff77e9',
-            'bermuda': '#78dcca',
-        },
 		extend: {
-
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'main-blue': '#F8F9FD',
+                'purple': '#3f3cbb',
+                'main-grey': '#B3B3B3',
+                'midnight': '#121063',
+                'metal': '#565584',
+                'tahiti': '#3ab7bf',
+                'silver': '#ecebff',
+                'bubble-gum': '#ff77e9',
+                'bermuda': '#78dcca',
+            },
         },
 	},
   plugins: [
