6c236c67e096de06127966620e114e8a4041ee2f
Charles <charles258@hotmail.fr>
2023-02-02 15:06:47 +0100

    Admin teams workflow

--- src/Form/UpdateEmployeeFormType.php
+++ src/Form/UpdateEmployeeFormType.php
@@ -4,25 +4,19 @@ namespace App\Form;
 
 use App\Entity\Employee;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
-
-class AdminRegistrationFormType extends AbstractType
+class UpdateEmployeeFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email')
-            ->add('firstname')
-            ->add('lastname')
             ->add('roles', ChoiceType::class, [
                 'required' => true,
                 'multiple' => false,
