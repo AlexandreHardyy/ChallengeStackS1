5bca28a16d00193c4e676732db1266773aa0b80c
Alexis TAN <alexistan9712@gmail.com>
2023-02-19 16:19:58 +0100

    ✨ Add Account page

--- src/Controller/Back/UpdateProfileController.php
+++ src/Controller/Back/UpdateProfileController.php
@@ -10,9 +10,37 @@ use App\Form\UpdateProfileType;
 use App\Entity\Employee;
 use App\Form\EmployeeType;
 use App\Repository\EmployeeRepository;
+use App\Form\UpdateProfilePictureType;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class UpdateProfileController extends AbstractController
 {
+    #[Route('/update/profile/picture', name: 'app_update_profile_picture')]
+    public function updatePicture(Request $request, EmployeeRepository $employeeRepository): Response
+    {
+        $employee = $this->getUser();
+
+        $form = $this->createForm(UpdateProfilePictureType::class, $employee);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $profileImageFile = $employee->getImageFile();
+            $profileImageFile = $form->get('imageFile')->getData();
+
+            if ($profileImageFile) {
+                // dd($profileImageFile);
+                $employee->setProfileImageName($profileImageFile);
+                // $employee->setImageName($profileImageFile->getClientOriginalName());
+            }
+
+            $employeeRepository->save($employee, true);
+            return $this->redirectToRoute('admin_app_update_profile_picture');
+        }
+
+        return $this->renderForm('back/update_profile_picture/index.html.twig', [
+            'updateProfilePictureForm' => $form
+        ]);
+    }
     #[Route('/update/profile/', name: 'app_update_profile')]
     public function edit(EmployeeRepository $employeeRepository, Request $request): Response
     {
