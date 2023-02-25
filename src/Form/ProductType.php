77ba2e6cd7536b08f49e983d75769672a61f4690
Alexandre Hardy <hardyalexandre2000@gmail.com>
2023-02-25 23:11:03 +0100

    category

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,6 +16,11 @@ class ProductType extends AbstractType
     {
         $builder
             ->add('title')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+            ])
             ->add('description')
             ->add('price')
             ->add('isActive')
